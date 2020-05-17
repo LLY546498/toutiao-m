b8b8025dfed3b035c937260d7c988a128fe336b3
LLY546498 <820954981@qq.com>
2020-05-17 18:12:39 +0800

    首页-搜索栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
  <template>
   <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+     class="app-nav-bar"
+    >
+    <van-button
+      class="search-btn"
+      slot="title"
+      icon="search"
+      type="info"
+      round
+      size="small"
+    >搜索</van-button>
+    </van-nav-bar>
     <!-- 文章频道列表 -->
       <van-tabs v-model="active">
         <van-tab
@@ -36,4 +49,22 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
+</style>
